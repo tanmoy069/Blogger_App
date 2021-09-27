ea4cadc56c0146f594a6567bb5a9b37e77c60240
Tanmoy Tushar <46753634+tanmoy069@users.noreply.github.com>
2021-09-27 23:28:30 +0600

    Blogger App projecct created

---
+++ src/test/java/com/springboot/bloggerapp/BloggerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.bloggerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
