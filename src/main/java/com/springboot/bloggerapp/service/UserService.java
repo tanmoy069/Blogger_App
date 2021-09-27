68c4d251e59ed232eca84eb0a5a01d10c98c8c07
tanmoy <pdt.tanmoy@gmail.com>
2021-09-28 00:25:52 +0600

    UserService has been created

---
+++ src/main/java/com/springboot/bloggerapp/service/UserService.java
@@ -0,0 +1,23 @@
+package com.springboot.bloggerapp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.bloggerapp.dao.UserDao;
+import com.springboot.bloggerapp.domain.User;
+
+@Service
+public class UserService {
+	
+	private UserDao userDao;
+	
+	@Autowired
+	public UserService(UserDao userDao) {
+		this.userDao = userDao;
+	}
+	
+	public User findUserByUsername(String username) {
+		return userDao.findUserByUsername(username);
+	}
+
+}
