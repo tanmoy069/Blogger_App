d008481be6b23805fb2f413f833651a591c703fc
tanmoy <pdt.tanmoy@gmail.com>
2021-10-01 13:38:43 +0600

    CommentService has been crated

---
+++ src/main/java/com/springboot/bloggerapp/service/CommentService.java
@@ -0,0 +1,53 @@
+package com.springboot.bloggerapp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.springboot.bloggerapp.dao.CommentDao;
+import com.springboot.bloggerapp.domain.Comment;
+
+public class CommentService extends AbstractService<Comment> {
+	
+	private CommentDao commentDao; 
+	
+	@Autowired
+	public CommentService(CommentDao commentDao) {
+		this.commentDao = commentDao;
+	}
+
+	@Override
+	public Comment findById(int id) {
+		return commentDao.findByCommentId(id);
+	}
+
+	@Override
+	public List<Comment> findAll() {
+		return commentDao.findAll();
+	}
+
+	@Override
+	public boolean save(Comment obj) {
+		try {
+			commentDao.save(obj);
+			LOGGER.info("Successfully post comment of blogid : " + obj.getBlogId());
+			return true;
+		} catch (Exception e) {
+			LOGGER.info("Failed to post");
+			return false;
+		}
+	}
+
+	@Override
+	public boolean update(Comment obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
