a26e15b94da731da9a9bd45bfb67ccb4918f98a1
tanmoy <pdt.tanmoy@gmail.com>
2021-10-01 13:29:29 +0600

    AbstractService has been implemented in UserSerivce

--- src/main/java/com/springboot/bloggerapp/service/UserService.java
+++ src/main/java/com/springboot/bloggerapp/service/UserService.java
@@ -1,5 +1,7 @@
 package com.springboot.bloggerapp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +9,7 @@ import com.springboot.bloggerapp.dao.UserDao;
 import com.springboot.bloggerapp.domain.User;
 
 @Service
-public class UserService {
+public class UserService extends AbstractService<User>{
 	
 	private UserDao userDao;
 	
@@ -20,11 +22,55 @@ public class UserService {
 		return userDao.findByUsername(username);
 	}
 	
+	@Override
 	public boolean save(User user) {
 		try {
 			userDao.save(user);
+			LOGGER.info("Successfully save username: " + user.getUsername());
 			return true;
 		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public User findById(int id) {
+		return userDao.findByUserId(id);
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userDao.findAll();
+	}
+
+	@Override
+	public boolean update(User obj) {
+		try {
+			if(findById(obj.getUserId()) != null) {
+				userDao.save(obj);
+				LOGGER.info("Successfully updated user: " + obj.getUserId());
+				return true;
+			}
+			LOGGER.info("Unable to update user, user doesn't exists");
+			return false;
+		} catch(Exception e) {
+			LOGGER.info("Failed to update user");
+			return false;
+		}
+	}
+
+	@Override
+	public boolean deleteById(int id) {
+		try {
+			if(findById(id) != null) {
+				userDao.deleteById(id);
+				LOGGER.info("Successfully deleted user: " + id);
+				return true;
+			}
+			LOGGER.info("Unable to delete user, user doesn't exists");
+			return false;
+		} catch(Exception e) {
+			LOGGER.info("Failed to delete user");
 			return false;
 		}
 	}
