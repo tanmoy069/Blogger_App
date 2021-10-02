e0581fd64a00eee610ce16c6aa6943f2b2acf088
tanmoy <pdt.tanmoy@gmail.com>
2021-10-03 00:59:08 +0600

    post method has been added to add comment on blog post's from home only

--- src/main/java/com/springboot/bloggerapp/controller/CommentController.java
+++ src/main/java/com/springboot/bloggerapp/controller/CommentController.java
@@ -1,14 +1,19 @@
 package com.springboot.bloggerapp.controller;
 
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.springboot.bloggerapp.domain.Comment;
 import com.springboot.bloggerapp.domain.User;
 import com.springboot.bloggerapp.service.BlogService;
 import com.springboot.bloggerapp.service.CommentService;
@@ -36,7 +41,21 @@ public class CommentController {
 		User user = userService.findUserByUsername(auth.getName());
 		model.addAttribute("blog", blogService.findById(blogId));
 		model.addAttribute("userDetails", user);
+		model.addAttribute("commentList", commentService.findByBlogId(blogId));
 		return "BlogToComments";
+	}
+	
+	@PostMapping("/by")
+	public String getBlogToComment(Model model,	@ModelAttribute("commentAddForm") Comment comment,
+			@RequestParam(name = "blogId", required = true) int blogId) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		if (!auth.isAuthenticated()) return "Login";
+		User user = userService.findUserByUsername(auth.getName());
+		System.out.println(comment.toString());
+		comment.setUsername(user.getUsername());
+		comment.setCreatedDate(Calendar.getInstance().getTime());
+		commentService.save(comment);
+		return "redirect:/blog/comments/by?blogId="+blogId;
 	}
 
 }
