a8f2fde2732e14f2a6c4a05a0ce73516537c850e
tanmoy <pdt.tanmoy@gmail.com>
2021-10-03 00:28:52 +0600

    findByBlogId() method has been added to CommentDao

--- src/main/java/com/springboot/bloggerapp/dao/CommentDao.java
+++ src/main/java/com/springboot/bloggerapp/dao/CommentDao.java
@@ -1,5 +1,7 @@
 package com.springboot.bloggerapp.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.bloggerapp.domain.Comment;
@@ -7,5 +9,6 @@ import com.springboot.bloggerapp.domain.Comment;
 public interface CommentDao extends JpaRepository<Comment, Integer>{
 	
 	Comment findByCommentId(int id);
+	List<Comment> findByBlogId(int blogId);
 
 }
