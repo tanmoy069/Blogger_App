8d66de576a5c87ba8c8206fdf6c98ef04f21d144
tanmoy <pdt.tanmoy@gmail.com>
2021-10-24 22:35:28 +0600

    UserRoleDao has been created

---
+++ src/main/java/com/springboot/bloggerapp/dao/UserRoleDao.java
@@ -0,0 +1,9 @@
+package com.springboot.bloggerapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.bloggerapp.domain.UserRole;
+
+public interface UserRoleDao extends JpaRepository<UserRole, Integer>{
+
+}
