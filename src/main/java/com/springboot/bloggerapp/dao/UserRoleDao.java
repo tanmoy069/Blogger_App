58bad311b636eda9e733c1e962cda9fedbeaaece
tanmoy <pdt.tanmoy@gmail.com>
2021-10-24 23:06:20 +0600

    findByRoleId() has been added to UserRoleDao

--- src/main/java/com/springboot/bloggerapp/dao/UserRoleDao.java
+++ src/main/java/com/springboot/bloggerapp/dao/UserRoleDao.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.bloggerapp.domain.UserRole;
 
 public interface UserRoleDao extends JpaRepository<UserRole, Integer>{
+	
+	UserRole findByRoleId(int roleId);
 
 }
