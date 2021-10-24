697485414e22bf520b2ed1fc5f20c9842dcd7992
tanmoy <pdt.tanmoy@gmail.com>
2021-10-24 22:22:31 +0600

    Category domain class has been created

---
+++ src/main/java/com/springboot/bloggerapp/domain/Category.java
@@ -0,0 +1,44 @@
+package com.springboot.bloggerapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String categoryName;
+
+	public Category(String categoryName) {
+		super();
+		this.categoryName = categoryName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCategoryName() {
+		return categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", categoryName=" + categoryName + "]";
+	}
+
+}
