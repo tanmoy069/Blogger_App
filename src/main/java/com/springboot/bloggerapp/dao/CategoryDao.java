5f4cce15110a73dd97c0b224a90c10d975cbca7d
tanmoy <pdt.tanmoy@gmail.com>
2021-10-24 22:50:46 +0600

    Added findById() method to CategoryDao

--- src/main/java/com/springboot/bloggerapp/dao/CategoryDao.java
+++ src/main/java/com/springboot/bloggerapp/dao/CategoryDao.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.springboot.bloggerapp.domain.Category;
 
 public interface CategoryDao extends JpaRepository<Category, Integer>  {
-
+	
+	Category findById(int id);
 }
