8006a9b3cfca8617ba0a86fbf36deb94162804b0
tanmoy <pdt.tanmoy@gmail.com>
2021-10-24 22:35:13 +0600

    CategoryDao has been created

---
+++ src/main/java/com/springboot/bloggerapp/dao/CategoryDao.java
@@ -0,0 +1,9 @@
+package com.springboot.bloggerapp.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springboot.bloggerapp.domain.Category;
+
+public interface CategoryDao extends JpaRepository<Category, Integer>  {
+
+}
